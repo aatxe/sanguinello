c23b2104c134a6a0e3d3dde02067dc6dc3b8ea79
Aaron Weiss <aweiss@hey.com>
2024-03-05 19:56:38 -0800

    reformat two tests

--- src/sgir/tests.rs
+++ src/sgir/tests.rs
@@ -9,19 +9,29 @@ fn test_kind_checking_trivial() {
 
 #[test]
 fn test_kind_checking_polymorphic_identity_function() {
-    let typ = Type::ForAll { parameters: vec![TypeBinding { id: "a".to_owned(), kind: Kind::Star }],
-                             typ: Box::new(Type::Function { arguments: vec![Type::Variable("a".to_owned())],
-                                                            result: Box::new(Type::Variable("a".to_owned())) }) };
+    let typ = Type::ForAll {
+        parameters: vec![TypeBinding { id: "a".to_owned(), kind: Kind::Star }],
+        typ: Box::new(Type::Function {
+            arguments: vec![Type::Variable("a".to_owned())],
+            result: Box::new(Type::Variable("a".to_owned()))
+        })
+    };
     let kind = check_kinds(&HashMap::new(), typ);
     assert_eq!(kind, Ok(Kind::Arrow { from: vec![Kind::Star], to: Box::new(Kind::Star) }));
 }
 
 #[test]
 fn test_kind_checking_instantiated_polymorphic_identity_function() {
-    let typ = Type::Instantiate { typ: Box::new(Type::ForAll { parameters: vec![TypeBinding { id: "a".to_owned(), kind: Kind::Star }],
-                                                               typ: Box::new(Type::Function { arguments: vec![Type::Variable("a".to_owned())],
-                                                                                              result: Box::new(Type::Variable("a".to_owned())) }) }),
-                                  arguments: vec![Type::Number] };
+    let typ = Type::Instantiate {
+        typ: Box::new(Type::ForAll {
+            parameters: vec![TypeBinding { id: "a".to_owned(), kind: Kind::Star }],
+            typ: Box::new(Type::Function {
+                arguments: vec![Type::Variable("a".to_owned())],
+                result: Box::new(Type::Variable("a".to_owned()))
+            })
+        }),
+        arguments: vec![Type::Number]
+    };
     let kind = check_kinds(&HashMap::new(), typ);
     assert_eq!(kind, Ok(Kind::Star));
 }
@@ -45,11 +55,19 @@ fn test_kind_checking_instantiated_monomorphic_type() {
 
 #[test]
 fn test_kind_checking_instantiated_polymorphic_function_with_type_constructor() {
-    let typ = Type::Instantiate { typ: Box::new(Type::ForAll { parameters: vec![TypeBinding { id: "a".to_owned(), kind: Kind::Star }],
-                                                               typ: Box::new(Type::Function { arguments: vec![Type::Variable("a".to_owned())],
-                                                                                              result: Box::new(Type::Variable("a".to_owned())) }) }),
-                                  arguments: vec![Type::ForAll { parameters: vec![TypeBinding { id: "b".to_owned(), kind: Kind::Star }],
-                                                                 typ: Box::new(Type::Variable("b".to_owned())) }] };
+    let typ = Type::Instantiate {
+        typ: Box::new(Type::ForAll {
+            parameters: vec![TypeBinding { id: "a".to_owned(), kind: Kind::Star }],
+            typ: Box::new(Type::Function {
+                arguments: vec![Type::Variable("a".to_owned())],
+                result: Box::new(Type::Variable("a".to_owned()))
+            })
+        }),
+        arguments: vec![Type::ForAll {
+            parameters: vec![TypeBinding { id: "b".to_owned(), kind: Kind::Star }],
+            typ: Box::new(Type::Variable("b".to_owned()))
+        }]
+    };
     let kind = check_kinds(&HashMap::new(), typ);
     assert_eq!(kind, Err(TypeError::KindMismatch { expected: Kind::Star, found: Kind::Arrow { from: vec![Kind::Star], to: Box::new(Kind::Star) }}));
 }
